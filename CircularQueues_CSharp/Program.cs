44c82f30370c24c7306bd1e625c9a167098f36d9
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2022-12-14 17:16:35 +0700

    menambahkan fungsi switch dan case 1 dan 2

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -124,6 +124,22 @@ namespace CircularQueues_CSharp
                     Console.Write("\nEnter your choice: ");
                     ch = Convert.ToChar(Console.ReadLine());
                     Console.WriteLine();
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                Console.Write("Enter a number: ");
+                                int num = Convert.ToInt32(System.Console.ReadLine());
+                                Console.WriteLine();
+                                q.insert(num);
+                            }
+                            break;
+                        case '2':
+                            {
+                                q.remove();
+                            }
+                            break;
+                    }
                 }
                 catch (Exception e)
                 {
